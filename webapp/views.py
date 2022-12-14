e335c9b7d3d7193f00fc4e650c2b431d31b5f149
Andrea <andreagenovese.management@gmail.com>
2022-12-14 17:13:05 +0100

    Modified HTML file to display articles on the site

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Article
+from django.utils import timezone
 
 def article_list(request):
-    return render(request, 'webapp/article_list.html', {})
+    articles = Article.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'webapp/article_list.html', {'articles': articles})
 
 # Create your views here.
